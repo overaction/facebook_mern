eeee9470bef07cb20d138570a045df16ad9c9ddd
overaction <52850392+overaction@users.noreply.github.com>
2020-12-29 15:44:03 +0900

    2. bcryptjs를 사용한 비밀번호 암호화

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,11 +4,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-router.get('/',(req,res) => {
-    res.send("Good?");
-});
+const bcrypt = require('bcryptjs');
 
-router.post('/signin', (req,res) => {
+router.post('/signup', (req,res) => {
     const {name,email,password} = req.body;
     if(!email || !password) {
         return res.status(422).json({error: "please add email or password"})
@@ -17,13 +15,23 @@ router.post('/signin', (req,res) => {
     .then((savedUser) => {
         if(savedUser)
             return res.status(422).json({error: "already exist"});
-        const newUser = new User({
-            name,
-            email,
-            password
-        });
-        newUser.save();
+        bcrypt.hash(password,12)
+        .then(hashed => {
+            const newUser = new User({
+                name,
+                email,
+                password: hashed
+            });
+            newUser.save()
+            .then(user => {
+                res.json({message: `saved successfully`});
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        })
     })
+    .catch(err => console.log(err));
 })
 
 module.exports = router;
